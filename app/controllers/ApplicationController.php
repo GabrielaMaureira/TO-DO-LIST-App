392ea075cd41a31ff206f47a7581afb248d47efb
Gabriela Maureira <ellamaureira@MacBook-Pro-de-Gabriela.local>
2023-02-20 12:10:26 +0100

    Adding create method, action, view and route

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -4,7 +4,30 @@
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
-{
-	
+
+ require_once(ROOT_PATH . '/app/models/TaskModel.class.php');
+ 
+
+class ApplicationController extends Controller {
+
+	public function indexAction(){
+        $this->taskListAction();
+    }
+    
+    public function taskListAction(){
+		$model = new TaskModel();
+        $this->view->content = $model->getTasks();
+	}
+
+    public function createTaskAction(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $model = new TaskModel();
+            $model->createTask($_POST['task'], 
+                                $_POST['status'], 
+                                $_POST['startDate'], 
+                                $_POST['endDate'] );
+        header('Location: ' . $this->_baseUrl());
+
+        }
+    }
 }
